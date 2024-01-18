a0fd08a088b66654a3b8d53050a0d3e64918ded4
clairemuiru1 <muiruclaire@gmail.com>
2024-01-18 14:55:22 +0300

    "Done with Assingment"

--- server/app.py
+++ server/app.py
@@ -48,6 +48,35 @@ class ShowArticle(Resource):
 
         return {'message': 'Maximum pageview limit reached'}, 401
 
+class Login(Resource):
+    def post(self):
+        username = request.get_json()['username']
+        user = User.query.filter(User.username == username).first()
+
+        if user:
+            session['user_id'] = user.id
+
+            return user.to_dict(), 200
+        else:
+            return {'message': 'User not found'}, 404
+        
+class Logout(Resource):
+    def delete(self):
+        session['user_id'] = None
+        return {'message':'204:No Content'}, 204
+    
+class CheckSession(Resource):
+    def get(self):
+        user = User.query.filter(User.id == session.get('user_id')).first()
+
+        if user:
+            return jsonify(user.to_dict()),200
+        else:
+            return {'message':'401:Not Authorized'},401
+        
+api.add_resource(CheckSession, '/check_session')
+api.add_resource(Logout, '/logout')
+api.add_resource(Login, '/login')
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
